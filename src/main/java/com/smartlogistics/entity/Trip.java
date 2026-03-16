364d90de134e87e9cef93b4b7acd174368274723
VISHAL REDDY <VISHALREDDY.com>
2026-03-16 22:49:15 +0530

    Fix user isolation and remove invalid UserIdEntityListener

--- src/main/java/com/smartlogistics/entity/Trip.java
+++ src/main/java/com/smartlogistics/entity/Trip.java
@@ -2,7 +2,6 @@ package com.smartlogistics.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,11 +9,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-import com.smartlogistics.entity.listener.UserIdEntityListener;
-
 @Entity
 @Table(name = "trips")
-@EntityListeners(UserIdEntityListener.class)
 public class Trip {
 
     @Id
